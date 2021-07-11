a49209be4620b9f6ffe2e53c70c636654a4f25ad
Austin Bowers <austin@solarreviews.com>
2021-07-11 01:40:20 -0400

    Implement Tailwind CSS framework. Update styling to dark theme for now. (Light/Dark mode coming soon)

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <CurrentTempDisplay/>
-      <RfRemote/>
+      <div className="bg-gray-700 dark:text-gray-100 dark:bg-gray-800 h-screen">
+        <div className="container max-w-screen-lg mx-auto p-4">
+          <CurrentTempDisplay/>
+          <RfRemote/>
+        </div>
+      </div>
     </div>
   );
 }
